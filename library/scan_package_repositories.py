374b0ef755e4609bbd16684ebe466ae093653c6a
Andrew J Huffman <ahuffman@redhat.com>
2019-07-24 11:36:22 -0400

    scan_package_repositories - WIP

---
+++ library/scan_package_repositories.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+
+# Copyright: (c) 2019, Andrew J. Huffman <ahuffman@redhat.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = '''
+---
+module: scan_package_repositories
+short_description: Collects all package repositories on a system
+version_added: "2.8"
+description:
+    - "Collects the repository data from package repositories on a system."
+    - "This module presents the package repository data and returns the configuration data as ansible_facts"
+output_ps_stdout_lines:
+    description:
+        - Whether or not to output the collected standard out lines from the 'ps auxww' command
+    default: False
+    required: False
+output_parsed_processes:
+    description:
+        - Whether or not to output parsed data from the 'ps auxww' command.
+    default: True
+    required: False
+author:
+    - Andrew J. Huffman (@ahuffman)
+'''
+
+EXAMPLES = '''
+
+'''
+
+RETURN = '''
+
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+import ansible.module_utils.facts.system.distribution as dist
+import os, re, platform
+from os.path import isfile, isdir, join
+
+def main():
+    module_args = dict(
+        output_ps_stdout_lines=dict(
+            type='bool',
+            default=False,
+            required=False
+        ),
+        output_parsed_processes=dict(
+            type='bool',
+            default=True,
+            required=False
+        )
+    )
+
+    result = dict(
+        changed=False,
+        original_message='',
+        message=''
+    )
+
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    params = module.params
+
+    def get_yum_repodata():
+        yum_repos = list()
+        return yum_repos
+
+
+    # def get_deb_repodata():
+
+
+    # Do work
+    ## Get OS type
+    # if platform.system().lower().startswith('linux'):
+    #     os_type = list(platform.linux_distribution())[0].lower()
+    # else:
+    #     os_type = platform.system().lower()
+    distro_facts = dist.DistributionFactCollector()
+    os_facts = distro_facts.collect(module)
+    os_family = os_facts['os_family']
+
+    ## Enterprise Linux - based
+    # if os_family == "RedHat":
+    #     repos = get_yum_repodata()
+
+    ## Debian Linux - based
+
+
+
+    # Build output
+    repositories = dict()
+    repositories['os_family'] = os_family
+    # repositories['repos'] = repos
+
+    result = {'ansible_facts': {'package_repositories': repositories }}
+
+    module.exit_json(**result)
+
+
+if __name__ == '__main__':
+    main()

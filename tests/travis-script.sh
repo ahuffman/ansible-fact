18b4e3a1a1f64653a63852dd3fbcab4de917f15b
John Westcott IV <john.westcott.iv@redhat.com>
2019-08-29 06:05:49 -0400

    Testing paths

--- tests/travis-script.sh
+++ tests/travis-script.sh
@@ -4,6 +4,11 @@ echo "Running on Ansible version"
 ansible-playbook --version
 echo "------------------------------------------"
 
+# TEMP Travis Testing
+echo "****************** TEMP TESTING ************************"
+ls /root/.ansible/collections/ansible_collections/john_westcott_iv/ansible_fact
+echo "****************** END TEMP TESTING ************************"
+
 cd /root/ansible-fact/tests/
 # display working dir
 pwd
